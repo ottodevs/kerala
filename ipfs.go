a64be89944a5eaf3168c99737acb2f099e8ca9e9
Otto G <ottodevs@gmail.com>
2017-04-10 18:08:59 +0200

    context

--- ipfs.go
+++ ipfs.go
@@ -4,7 +4,7 @@ import (
 
     "github.com/ipfs/go-ipfs/core"
     "github.com/ipfs/go-ipfs/repo/fsrepo"
-    context
+    "context"
 	"fmt"
 	u "github.com/ipfs/go-ipfs/util"
 	merkledag "github.com/go-ipfs/merkledag"
